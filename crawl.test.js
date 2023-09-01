1cd4afdd3b8f86e7fd649a099dbe91e7850129b1
Nathan Owens <nmowens95@gmail.com>
2023-09-01 15:08:47 -0500

    Added more tests. Practicing TDD using jest.

---
+++ crawl.test.js
@@ -0,0 +1,30 @@
+const { normalizeURL } = require('./crawl.js')
+const { test, expect } = require('@jest/globals')
+
+test('normalizeUrl strip protocol', () => {
+    const input = 'https://blog.boot.dev/path'
+    const actual = normalizeURL(input)
+    const expected = 'blog.boot.dev/path'
+    expect(actual).toEqual(expected)
+})
+
+test('normalizeUrl strip trailing /', () => {
+    const input = 'https://blog.boot.dev/path/'
+    const actual = normalizeURL(input)
+    const expected = 'blog.boot.dev/path'
+    expect(actual).toEqual(expected)
+})
+
+test('normalizeUrl capitals', () => {
+    const input = 'https://Blog.boot.dev/path/'
+    const actual = normalizeURL(input)
+    const expected = 'blog.boot.dev/path'
+    expect(actual).toEqual(expected)
+})
+
+test('normalizeUrl strip http', () => {
+    const input = 'https://blog.boot.dev/path/'
+    const actual = normalizeURL(input)
+    const expected = 'blog.boot.dev/path'
+    expect(actual).toEqual(expected)
+})
