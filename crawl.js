a5023c8338bd55a2bb24655f694c3d0a1b66aa9d
Nathan Owens <nmowens95@gmail.com>
2023-09-05 11:14:16 -0500

    Added try catch for errors in pages to be crawled

--- crawl.js
+++ crawl.js
@@ -1,5 +1,16 @@
 const { JSDOM } = require('jsdom')
 
+async function crawlPage(currentURL){
+    console.log(`actively crawling: ${currentURL}`)
+    
+    try {
+        const resp = await fetch(currentURL)
+        console.log(await resp.text())
+    } catch(err) {
+        console.log(`error in fetch: ${err.message}, on page: ${currentURL}`)
+    }
+}
+
 function getURLsFromHTML(htmlBody, baseURL) {
     const urls = []
     const dom = new JSDOM(htmlBody)
@@ -38,5 +49,6 @@ function normalizeURL(urlString) {
 
 module.exports = {
     normalizeURL,
-    getURLsFromHTML
+    getURLsFromHTML,
+    crawlPage
 }
